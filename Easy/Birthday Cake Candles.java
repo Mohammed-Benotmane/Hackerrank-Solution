960c5a371b3da997746821875c6b936b5b0a9ec8
Benotmane Mohammed <39698080+Mohammed-Benotmane@users.noreply.github.com>
2018-12-02 00:04:41 +0100

    max score: 10

---
+++ Easy/Birthday Cake Candles.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+        int max=ar[0];
+        for(int i=1;i<ar.length;i++){
+         if(max<ar[i]){
+             max=ar[i];
+         }   
+        }
+        int nb = 0;
+        for(int j=0;j<ar.length;j++){
+            if(ar[j]==max){
+                nb++;
+            }
+        }
+        return nb;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
