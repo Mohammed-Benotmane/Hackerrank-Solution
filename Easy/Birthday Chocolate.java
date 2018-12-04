f52dae325427a92067b4d0179814fc7cdcd4fddc
Benotmane Mohammed <39698080+Mohammed-Benotmane@users.noreply.github.com>
2018-12-04 13:12:40 +0100

    max score: 10

---
+++ Easy/Birthday Chocolate.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static int solve(int[] s, int d, int m) {
+        int res=0;
+        for(int i=0;i<=(s.length)-m;i++){
+            int temp=0;
+            for(int j=i;j<i+m;j++){
+              temp+=s[j];  
+            }
+            if(temp==d){
+                res++;
+            }
+        }
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] s = new int[n];
+
+        String[] sItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int sItem = Integer.parseInt(sItems[i]);
+            s[i] = sItem;
+        }
+
+        String[] dm = scanner.nextLine().split(" ");
+
+        int d = Integer.parseInt(dm[0]);
+
+        int m = Integer.parseInt(dm[1]);
+
+        int result = solve(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
