c567798ca436ef655d12d22454027b29691d7f34
Benotmane Mohammed <39698080+Mohammed-Benotmane@users.noreply.github.com>
2018-12-05 22:57:20 +0100

    max score: 10

---
+++ Easy/Diagonal Difference.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] a) {
+        int p = 0;
+        int s = 0;
+        for(int i=0;i<a.length;i++){
+            p += a[i][i];
+            s += a[i][(a.length)-i-1];
+        }
+        if(p>s){
+        return p-s;
+        }
+        else{
+        return s-p;
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] a = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] aRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int aItem = Integer.parseInt(aRowItems[j]);
+                a[i][j] = aItem;
+            }
+        }
+
+        int result = diagonalDifference(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
