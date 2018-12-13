96aa379614c861aa70206e37c280713032ad1a24
Benotmane Mohammed <39698080+Mohammed-Benotmane@users.noreply.github.com>
2018-12-13 19:59:17 +0100

    max score: 10

---
+++ Easy/Compare the Triplets.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static int[] solve(int[] a, int[] b) {
+       int[] m = new int[2];
+        m[0]= 0;
+        m[1]= 0;
+        for(int i = 0 ; i < 3; i++){
+            if(a[i] < b[i]){
+                m[1]++;
+            }
+            else{
+            if(a[i] > b[i]){
+                m[0]++;
+            }
+            }
+        }
+        return m;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[] a = new int[3];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] b = new int[3];
+
+        String[] bItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bItems[i]);
+            b[i] = bItem;
+        }
+
+        int[] result = solve(a, b);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
