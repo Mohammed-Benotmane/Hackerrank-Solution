71361423735e14232a985069e855ebdb0f888865
Benotmane Mohammed <mohammedbenotmanetom@gmail.com>
2020-07-03 00:07:14 +0200

    Add files via upload

---
+++ 30 Days Of Code/Day 13 Abstract Classes.java
@@ -0,0 +1,13 @@
+    class MyBook extends Book{
+        int price;
+
+        MyBook(String title,String author,int price){
+            super(title,author);
+            this.price=price;
+        }
+        void display(){
+            System.out.println("Title: "+title);
+            System.out.println("Author: "+author);
+            System.out.println("Price: "+price);
+        }
+    }
