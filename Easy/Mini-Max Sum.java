7219632ac7b2d0da849403c61e0cf7d4fb29a56a
Tighrine <aghiles.tighrine2@gmail.com>
2020-12-24 13:20:47 +0100

    Update Mini-Max Sum.java
    With this changes we will get an O(n) complexity

--- Easy/Mini-Max Sum.java
+++ Easy/Mini-Max Sum.java
@@ -11,12 +11,12 @@ public class Solution {
     // Complete the miniMaxSum function below.
     static void miniMaxSum(int[] arr) {
         long[] m = new long[arr.length];
+        long sum = 0;
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(j != i){
-                    m[i]+=arr[j];
-                }
-            }
+            sum += arr[i]
+        }
+        for(int i = 0; i < arr.lenght; i++){
+            m[i] = sum - arr[i]
         }
         long min = m[0];
         long max = m[0];
