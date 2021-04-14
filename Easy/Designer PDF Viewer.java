0e1701621157504f81c394a15fbce1c0910afef5
Benotmane Mohammed <mohammedbenotmanetom@gmail.com>
2021-04-14 16:46:34 +0100

    Add files via upload

---
+++ Easy/Designer PDF Viewer.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int designerPdfViewer(int[] h, String word) {
+        int max = h[word.charAt(0)-97];
+        for(int i = 1;i<word.length();i++){
+            if(max<h[word.charAt(i)-97]){
+                max = h[word.charAt(i)-97];
+            }
+        }
+        
+        return word.length() * max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[] h = new int[26];
+
+        String[] hItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 26; i++) {
+            int hItem = Integer.parseInt(hItems[i]);
+            h[i] = hItem;
+        }
+
+        String word = scanner.nextLine();
+
+        int result = designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
