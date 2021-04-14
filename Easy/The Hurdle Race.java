8eed91e7429a95a1be1ac336e74ba43be4df9aac
Benotmane Mohammed <mohammedbenotmanetom@gmail.com>
2021-04-14 16:26:03 +0100

    Add files via upload

---
+++ Easy/The Hurdle Race.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int hurdleRace(int k, int[] height) {
+        int max = height[0];
+        for(int i =1;i<height.length;i++){
+            if(max<height[i]){
+                max=height[i];
+            }
+        }
+        if(max>k){
+            return max-k;
+        }
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] height = new int[n];
+
+        String[] heightItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int heightItem = Integer.parseInt(heightItems[i]);
+            height[i] = heightItem;
+        }
+
+        int result = hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
